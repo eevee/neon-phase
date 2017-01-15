a643969bc120e965e7bf0dc623852b5483cfd38b
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2017-01-15 13:40:50 -0800

    Implement colorbirds, via much copy/pasting

--- neonphase/actors/critters.lua
+++ neonphase/actors/critters.lua
@@ -105,10 +105,141 @@ function DustBunny:update(dt)
     -- TODO how do these work for things that aren't players?
     self.sprite:set_facing_right(not self.facing_left)
     self.sprite:set_pose(pose)
-
-    local hits = self._stupid_hits_hack
-    debug_hits = hits
 end
+
+
+local Colorbird1 = actors_base.MobileActor:extend{
+    name = 'color bird 1',
+    sprite_name = 'color bird 1',
+
+    xaccel = 100,
+    friction = 50,
+    max_speed = 10,
+    gravity_multiplier = 0.25,
+
+    -- Conscious movement decisions
+    decision_walk = 0,
+    decision_jump_mode = 0,
+}
+
+function Colorbird1:init(...)
+    Colorbird1.__super.init(self, ...)
+
+    self.pos0 = self.pos
+    self.flutter_amplitude = math.random(32, 96)
+    self.flutter_frequency = math.random() * 2 + 1
+end
+
+function Colorbird1:blocks()
+    return false
+end
+
+function Colorbird1:think()
+    if self.busy then
+        return
+    end
+
+    if love.math.random() < 0.5 then
+        self.decision_walk = -1
+    else
+        self.decision_walk = 1
+    end
+
+    self.busy = true
+    worldscene.tick:delay(function() self.decision_walk = 0 end, 1):after(function() self.busy = false end, 3)
+end
+
+-- FIXME merge this with...  literally anything else
+function Colorbird1:update(dt)
+    if self.is_dead then
+        -- FIXME a corpse still has physics, just not input
+        self.sprite:update(dt)
+        return
+    end
+
+    -- Make some decisions
+    self:think()
+
+    local xmult
+    if self.on_ground then
+        -- TODO adjust this factor when on a slope, so ascending is harder than
+        -- descending?  maybe even affect max_speed going uphill?
+        xmult = self.ground_friction
+    else
+        xmult = self.aircontrol
+    end
+    --print()
+    --print()
+    --print("position", self.pos, "velocity", self.velocity)
+
+    -- Explicit movement
+    local pose = 'stand'
+    -- FIXME this doesn't at all smartly transition between fluttering and
+    -- walking, which is particularly bad when a bird can fall off the perch it
+    -- starts on
+    if not self.on_ground then
+        self.velocity.x = - self.flutter_amplitude * self.flutter_frequency * math.sin(self.flutter_frequency * self.timer)
+        if self.pos.y > self.pos0.y then
+            self.velocity.y = -32
+        end
+        self.facing_left = self.velocity.x < 0
+        pose = 'jump'
+    elseif self.decision_walk > 0 then
+        if self.velocity.x < self.max_speed then
+            self.velocity.x = math.min(self.max_speed, self.velocity.x + self.xaccel * xmult * dt)
+        end
+        self.facing_left = false
+        pose = 'walk'
+    elseif self.decision_walk < 0 then
+        if self.velocity.x > -self.max_speed then
+            self.velocity.x = math.max(-self.max_speed, self.velocity.x - self.xaccel * xmult * dt)
+        end
+        self.facing_left = true
+        pose = 'walk'
+    end
+
+    -- Jumping
+    -- [n/a]
+
+    -- Run the base logic to perform movement, collision, sprite updating, etc.
+    Colorbird1.__super.update(self, dt)
+
+    -- FIXME uhh this sucks, but otherwise the death animation is clobbered by
+    -- the bit below!  should death skip the rest of the actor's update cycle
+    -- entirely, including activating any other collision?  should death only
+    -- happen at the start of a frame?  should it be an event or something?
+    if self.is_dead then
+        return
+    end
+
+    -- Update pose depending on actual movement
+    if self.on_ground then
+    elseif self.velocity.y < 0 then
+        --pose = 'jump'
+    elseif self.velocity.y > 0 then
+        --pose = 'fall'
+    end
+    -- TODO how do these work for things that aren't players?
+    self.sprite:set_facing_right(not self.facing_left)
+    self.sprite:set_pose(pose)
+end
+
+local Colorbird2 = Colorbird1:extend{
+    name = 'color bird 2',
+    sprite_name = 'color bird 2',
+}
+local Colorbird3 = Colorbird1:extend{
+    name = 'color bird 3',
+    sprite_name = 'color bird 3',
+}
+local Colorbird4 = Colorbird1:extend{
+    name = 'color bird 4',
+    sprite_name = 'color bird 4',
+}
+local Colorbird5 = Colorbird1:extend{
+    name = 'color bird 5',
+    sprite_name = 'color bird 5',
+}
 
 
 local Glomeleon = actors_base.MobileActor:extend{
