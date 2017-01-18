649b3f57ff362e17d05ce11f9c015077275616b6
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2017-01-17 23:21:24 -0800

    Correct required LÖVE version

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.title = "NEON PHASE"
-    t.version = "0.10.1"
+    t.version = "0.10.2"
     t.window.width = 640
     t.window.height = 360
     t.window.icon = "assets/images/icon.png"
